52d157fe21ab27b041ff371b73d393b94d028b5a
alexandre.lloret <alloret.perso@gmail.com>
2020-04-29 10:52:42 +0200

    First commit

---
+++ src/main/java/com/learning/spring/boot/fundamentals/FundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.spring.boot.fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/spring/boot/fundamentals/FundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring.boot.fundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
